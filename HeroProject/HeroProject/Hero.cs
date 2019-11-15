0b31b5bed6d012c56eac6e74f53a3e8e26f5a3d7
erikparisva <erikparisva@gmail.com>
2019-11-15 09:14:22 +0200

    OOOOOOOOOOOOOGA

---
+++ HeroProject/HeroProject/Hero.cs
@@ -0,0 +1,25 @@
+﻿namespace HeroProject
+{
+    class Hero
+    {
+        private string _name;
+        private string _location;
+
+        public string Name { get => _name; set => _name = value; }
+        public string Location { get => _location; set => _location = value; }
+
+        public Hero(string name, string location)
+        {
+            _name = name;
+            Location = location;
+        }
+        public int rescue(int inDanger)
+        {
+            return (int)(inDanger * 0.95);
+        }
+        public override string ToString()
+        {
+            return $"Hero: {_name}\nLocation: {_location}";
+        }
+    }
+}
